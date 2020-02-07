f4e6d864ed4e070a30a7040fb42b2f68c30ca6f4
Andres Quesada <andres@marxentlabs.com>
2020-02-07 05:37:04 -0500

    Fixed script so it ide is happy and so it pipeline

--- src/main/groovy/io/open/cloud/jenkins/BuildPolicyEnforcer.groovy
+++ src/main/groovy/io/open/cloud/jenkins/BuildPolicyEnforcer.groovy
@@ -2,18 +2,46 @@ package io.open.cloud.jenkins
 
 import jenkins.model.Jenkins
 
-class BuildPolicyEnforcer {
+final def daysToKeep = 30
+final def numToKeep = 10
+final def artifactDaysToKeep = 5
+final def artifactNumToKeep = 3
 
-    static void enforcePolicy () {
-        Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
-            if (job.isBuildable() && job.supportsLogRotator() && job.getProperty(jenkins.model.BuildDiscarderProperty) == null) {
-                println "Processing \"${job.fullDisplayName}\""
-                if ("${job.fullDisplayName}".equals('GCP--to--AWS--Storage')) {
-                    job.setBuildDiscarder(new hudson.tasks.LogRotator ( daysToKeep, numToKeep, artifactDaysToKeep, artifactNumToKeep))
-                    println "${job.fullName} is updated"
-                }
-            }
+def enforcePolicy = {
+    Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
+        def name = job.fullDisplayName
+        if (job.isBuildable() && job.supportsLogRotator() && job.getProperty(jenkins.model.BuildDiscarderProperty) == null) {
+
+            def buildable = job.isBuildable()
+            def logRotate = job.supportsLogRotator()
+            def discard = job.getProperty(jenkins.model.BuildDiscarderProperty)
+
+            println "--- Enforcing Policy --- "
+            println "Full display name: ${name}"
+            println "Buildable: ${buildable}"
+            println "Logs Rotate: ${logRotate}"
+            println "Discard: ${discard}"
+            job.setBuildDiscarder(new hudson.tasks.LogRotator ( daysToKeep, numToKeep, artifactDaysToKeep, artifactNumToKeep))
+            println "--- End Policy Enforcement"
         }
-        return;
+    }
+}
+
+def verifyPolicies = {
+    Jenkins.instanceOrNull.allItems(hudson.model.Job).each { job ->
+
+    }
+}
+node('master') {
+    stage('Print Policy') {
+        println '--- Policy ---'
+        println 'daysToKeep : 30'
+        println 'numToKeep = 10'
+        println 'artifactDaysToKeep = 5'
+        println 'artifactNumToKeep = 3'
+        println '--- End Policy ---'
+    }
+    stage('Enforce Policy') {
+        enforcePolicy()
     }
 }
